2d5ed8b5215c1aa31c477fde69d4cc1ba650d753
AchiengOreyo <smartresearcher@yahoo.com>
2022-07-05 10:28:35 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints dog struct
+ * @d: pointer to dog struct
+ */
+void print_dog(struct dog *d)
+{
+if (d)
+{
+d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+printf("Age: %f\n", d->age);
+d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,2 +1,3 @@
 Task 0 -defines a type struct dog with the following elements:   name, type = char *    age, type = float     owner, type = char *
 Task 1 -  initialize a variable of type struct dog
+Task 2 - prints a struct dog
